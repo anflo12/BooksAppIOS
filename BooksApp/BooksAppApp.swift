7f0d636ed6312aad48d3c7579fc19375e8e202cf
andres florez <andresflopaez@gmail.com>
2022-01-19 10:41:12 -0500

    Initial Commit

---
+++ BooksApp/BooksAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAppApp.swift
+//  BooksApp
+//
+//  Created by Andrés Flórez on 19/01/22.
+//
+
+import SwiftUI
+
+@main
+struct BooksAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
